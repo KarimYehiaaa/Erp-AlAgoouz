746f045500676ff5245996784ffcb865c15f51f7
KarimYehiaaa <karimyihia@gmail.com>
2026-07-12 15:23:11 +0300

    chore: test raw pg client connection in debug endpoint

--- api/debug-env.js
+++ api/debug-env.js
@@ -1,12 +1,20 @@
 import config from '../backend/src/config/index.js';
-import pool from '../backend/src/database/pool.js';
+import pg from 'pg';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  let dbError = null;
+  const client = new pg.Client({ connectionString: config.db.connectionString, ssl: { rejectUnauthorized: false } });
+  try {
+    await client.connect();
+    await client.end();
+    dbError = 'SUCCESS';
+  } catch (err) {
+    dbError = err.message + ' | host: ' + client.host + ' | port: ' + client.port;
+  }
+
   res.status(200).json({
     DB_HOST: process.env.DB_HOST,
-    PGHOST: process.env.PGHOST,
     configConnectionString: config.db.connectionString ? config.db.connectionString.replace(/:[^:]+@/, ':***@') : 'UNDEFINED',
-    poolHost: pool.options.host,
-    poolConnectionString: pool.options.connectionString ? pool.options.connectionString.replace(/:[^:]+@/, ':***@') : 'UNDEFINED'
+    dbError
   });
 }
